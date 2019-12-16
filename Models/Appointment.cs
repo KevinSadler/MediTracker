5c7a15604970540184369982640946fcc1e0f350
KevinSadler <KevinSadler@github.comgit config --global user.email KevinSadler@github.comgit config --global user.email KevinSadler@github.com>
2019-12-16 14:39:48 -0600

    Full CRUD for appointments. User has ability to view list of appointments by Upcoming or Previous. Details view conditionally renders notes. Back to list button on Details view is conditionally rendered depending on whether the current appointment is upcoming or previous

--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -26,5 +27,6 @@ namespace MediTracker.Models
         public ApplicationUser User { get; set; }
 
         public List<Note> Notes { get; set; }
+
     }
 }
