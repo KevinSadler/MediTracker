0ea632f12ad3cc384b8de97fc264e8be3bceb6f2
KevinSadler <KevinSadler@github.comgit config --global user.email KevinSadler@github.comgit config --global user.email KevinSadler@github.com>
2019-12-16 11:48:03 -0600

    Full CRUD for symptoms. User can create a new symptom when creating an entry. Deleting a symptom will delete all occurences from user's journal entries

---
+++ Controllers/SymptomsController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MediTracker.Data;
+using MediTracker.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace MediTracker.Controllers
+{
+    public class SymptomsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public SymptomsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Symptoms
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            var applicationDbContext = _context.Symptoms.Where(s => s.UserId == user.Id);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Symptoms/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var symptom = await _context.Symptoms
+                .Include(s => s.User)
+                .FirstOrDefaultAsync(m => m.SymptomId == id);
+            if (symptom == null)
+            {
+                return NotFound();
+            }
+
+            return View(symptom);
+        }
+
+        // GET: Symptoms/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Symptoms/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SymptomId,Title,UserId")] Symptom symptom)
+        {
+            ModelState.Remove("User");
+            ModelState.Remove("UserId");
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(HttpContext.User);
+                symptom.User = user;
+                symptom.UserId = user.Id;
+                _context.Add(symptom);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(symptom);
+        }
+
+        // GET: Symptoms/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var symptom = await _context.Symptoms.FindAsync(id);
+            if (symptom == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", symptom.UserId);
+            return View(symptom);
+        }
+
+        // POST: Symptoms/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SymptomId,Title,UserId")] Symptom symptom)
+        {
+            if (id != symptom.SymptomId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(symptom);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SymptomExists(symptom.SymptomId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", symptom.UserId);
+            return View(symptom);
+        }
+
+        // GET: Symptoms/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var symptom = await _context.Symptoms
+                .Include(s => s.User)
+                .FirstOrDefaultAsync(m => m.SymptomId == id);
+            if (symptom == null)
+            {
+                return NotFound();
+            }
+
+            return View(symptom);
+        }
+
+        // POST: Symptoms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var EntrySymptomsToDelete = await _context.EntrySymptoms.Where(es => es.SymptomId == id).ToListAsync();
+            foreach (var es in EntrySymptomsToDelete) {
+                _context.EntrySymptoms.Remove(es);
+            }
+            var symptom = await _context.Symptoms.FindAsync(id);
+            _context.Symptoms.Remove(symptom);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SymptomExists(int id)
+        {
+            return _context.Symptoms.Any(e => e.SymptomId == id);
+        }
+    }
+}
